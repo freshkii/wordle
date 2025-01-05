1be87acc4da28907104000d6dc4dc71d99636e5c
freskey <frowkzey@gmail.com>
2025-01-05 11:16:46 +0100

    polishing

--- Makefile
+++ Makefile
@@ -1,5 +1,25 @@
+BINDIR = /usr/local/bin/
+
+ifeq ($(SUDO_USER),)
+  USER_HOME = $(HOME)
+else
+  USER_HOME = $(shell getent passwd $(SUDO_USER) | cut -d: -f6)
+endif
+
+DATADIR = $(USER_HOME)/.local/share/wordle
+
 all:
 	gcc main.c -o wordle
 
-.clean:
-	rm wordle
+install:
+	install -Dm755 wordle $(BINDIR)
+	mkdir $(DATADIR)
+	cp words.txt $(DATADIR)
+
+uninstall:
+	rm $(BINDIR)/wordle
+	rm -r $(DATADIR)
+
+
+clean:
+	rm -f wordle
--- main.c
+++ main.c
@@ -3,12 +3,15 @@
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
 
-#define RED "\x1B[0;31m"
-#define GREEN "\x1B[0;32m"
-#define YELLOW "\x1B[0;33m"
-#define BLUE "\x1B[0;34m"
-#define RESET "\x1B[0m"
+#define RED 		"\x1B[0;31m"
+#define GREEN 	"\x1B[0;32m"
+#define YELLOW 	"\x1B[0;33m"
+#define BLUE 		"\x1B[0;34m"
+#define RESET		"\x1B[0m"
+
+#define WORD_LEN 5
 
 #define green(X) GREEN X RESET
 
@@ -16,27 +19,68 @@ int main(int argc, char **argv) {
 	// greeting
 	printf(green("┌────────┐\n│") " Wordle " green("│\n└────────┘\n"));
 	char filename[256];
-	const int word_len = 5;
 
 	// input handling
-	if (argc == 2) {
-		if (strlen(argv[1]) <= 255) {
-			strcpy(filename, argv[1]);
-		}
-		else {
-			fprintf(stderr, RED "error: filename too long\n" RESET);
-			return 1;
-		}
-	}
-	else if (argc > 2) {
-		printf("usage: wordle [filename] or wordle\n");
-	}
-	else {
-		printf("filename: ");
-		scanf("%255s", filename);
-		printf(BLUE "(tip : next time, type 'wordle [filename]')\n" RESET);
-	}
+	switch(argc) {
+		case 1:
+			#ifdef __linux__
+				char *homedir = getenv("HOME");
+				char *basedir = malloc( sizeof(char) *
+						(strlen(homedir)+ strlen("/.local/share")) );
+				strcpy(basedir, homedir);
+				strcat(basedir, "/.local/share");
+			#endif
+			#ifdef __WIN32
+				char *homedir = getenv("APPDATA");
+				char *basedir = malloc( sizeof(char) *
+						(strlen(homedir)+ strlen("/Library/Application Support/")) );
+				strcpy(basedir, homedir);
+				strcat(basedir, "/Library/Application Support/");
+			#endif
+			#ifdef __POMME__
+				char *homedir = getenv("HOME");
+			#endif
 
+			if (!strcmp(homedir, "")) {
+					fprintf(stderr, RED "error: couldn't get enviromnent variable\n" RESET);
+					return 1;
+			}
+
+			strcpy(filename, basedir);
+			strncat(filename, "/wordle/words.txt", strlen(filename));
+			break;
+		case 2:
+			if (!strcmp(argv[1], "-f")) {
+				printf("usage:\n- wordle \n- wordle -f\n- wordle -f [filename]\n");
+				return 0;
+			}
+
+			printf("filename: ");
+			scanf("%255s", filename);
+			printf(BLUE "(tip : next time, type 'wordle [filename]')\n" RESET);
+			break;
+		case 3:
+			if (!strcmp(argv[1], "-f")) {
+				printf("usage:\n- wordle \n- wordle -f\n- wordle -f [filename]\n");
+				return 0;
+			}
+
+			if (strlen(argv[1]) > 255) {
+				printf("filename too long\n");
+				return 0;
+			}
+
+			char cwd[256];
+			if (getcwd(cwd,  sizeof(cwd)) == NULL){
+				fprintf(stderr, RED "error: coudln't get working directory\n" RESET);
+				return 1;
+			}
+
+			strcpy(filename, cwd);
+			strncat(filename, argv[1], 255-strlen(filename));
+			break;
+	}
+	
 	// file streaming
 	FILE* f = fopen(filename, "r");
 
@@ -48,10 +92,10 @@ int main(int argc, char **argv) {
 	// get amount of words
 	fseek(f, 0L, SEEK_END);
 	long file_size = ftell(f);
-	long length = file_size / (word_len + 1);
+	long length = file_size / (WORD_LEN + 1);
 	rewind(f);
 
-	char words[length][word_len+1];
+	char words[length][WORD_LEN+1];
 
 	for (int i = 0; i < length; i++) {
 		char *line = NULL;
@@ -60,9 +104,9 @@ int main(int argc, char **argv) {
 			fprintf(stderr, RED "error: a problem has occurred while reading the file\n" RESET);
 			return 1;
 		}
-		for (int j = 0; j < word_len; j++)
+		for (int j = 0; j < WORD_LEN; j++)
 			words[i][j] = line[j];
-		words[i][word_len] = '\0';
+		words[i][WORD_LEN] = '\0';
 	}
 
 	fclose(f);
@@ -72,13 +116,13 @@ int main(int argc, char **argv) {
 	int n = rand_r(&seed) % length;
 
 	char random_word[6];
-	strncpy(random_word, words[n], word_len);
+	strncpy(random_word, words[n], WORD_LEN);
 
 	char user_input[6] = "     ";
 	int i = 0;
 	for (; i < 5 && strcmp(user_input, random_word) != 0; i++) {
 		char tmp[6];
-		strncpy(tmp, random_word, word_len);
+		strncpy(tmp, random_word, WORD_LEN);
 		printf("%i attempts left:", 5-i);
 		scanf(" %5s", user_input);
 		char ok = 1;
@@ -94,11 +138,11 @@ int main(int argc, char **argv) {
 		if (ok == 0)
 			continue;
 
-		for (int i = 0; i < word_len; i++) {
+		for (int i = 0; i < WORD_LEN; i++) {
 			char c = user_input[i];
 			if (c == '\0' || !isalpha(c)) {
 			}
-			for (int j = 0; j < word_len; j++) {
+			for (int j = 0; j < WORD_LEN; j++) {
 				if (c == tmp[j]) {
 					tmp[j] = ' ';
 					if (i == j)
@@ -107,17 +151,16 @@ int main(int argc, char **argv) {
 						printf(YELLOW "%c" RESET, c);
 					break;
 				}
-				else if (j == word_len-1){
+				else if (j == WORD_LEN-1){
 					printf("%c", user_input[i]);
 				}
 			}
 		}
 		printf("\n");
 	}
-	i++;
 
 	if (strcmp(user_input, random_word) == 0)
-		printf(green("Congratulations !") " You've won within" green("%i") "attempt%c.\n", i, (i == 1 ? '\0' : 's'));
+		printf(green("Congratulations !") " You've won within " green("%i") " attempt%c.\n", i, (i == 1 ? '\0' : 's'));
 	printf("The word was %s.\n", random_word);
 
 	return 0;
