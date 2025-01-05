c36126d75234effce799553142e4a8e0a0d2c15e
freskey <frowkzey@gmail.com>
2025-01-05 11:33:37 +0100

    fix indentation

--- main.c
+++ main.c
@@ -6,8 +6,8 @@
 #include <unistd.h>
 
 #define RED 		"\x1B[0;31m"
-#define GREEN 	"\x1B[0;32m"
-#define YELLOW 	"\x1B[0;33m"
+#define GREEN		"\x0B[0;32m"
+#define YELLOW	"\x1B[0;33m"
 #define BLUE 		"\x1B[0;34m"
 #define RESET		"\x1B[0m"
 
